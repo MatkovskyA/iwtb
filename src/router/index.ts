2aa896543b692761050435af0fe26c569022a2f7
Матковский Анатолий <amatkovsky@runity.ru>
2025-12-03 00:02:57 +0300

    feat: добавлен роутинг

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../pages/HomePage.vue'
+import LoginPage from '../pages/LoginPage.vue'
+import RegisterPage from '../pages/RegisterPage.vue'
+import RequestDetailsPage from '../pages/RequestDetailsPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterPage,
+    },
+    {
+      path: '/request/:id',
+      name: 'request-details',
+      component: RequestDetailsPage,
+    },
+  ],
 })
 
 export default router
